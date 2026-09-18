3d22d3429593dc32aad88b7e9b3bc1c99a9f3ad2
arion <53127594+JustArion@users.noreply.github.com>
2026-09-18 19:07:12 +0200

    Move statics

---
+++ src/Dawn.Common.Windows/Extensions/StaticEx.cs
@@ -0,0 +1,44 @@
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace Dawn.Common.Windows.Extensions;
+
+[SuppressMessage("ReSharper", "InvokeAsExtensionMemberFromSameClass")]
+public static class StaticEx
+{
+    private static readonly Lazy<bool> isAdmin = new(() => new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator));
+    
+    extension(Static)
+    {
+        [DoesNotReturn]
+        public static void RestartAsAdmin(params string[] arguments)
+        {
+            StartAsAdmin(Environment.ProcessPath!, arguments);
+            
+            Environment.Exit(0);
+        }
+
+        public static void StartAsAdmin(string file, params string[] arguments)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = file,
+                Verb = "runas",
+                UseShellExecute = true,
+            };
+            foreach (var argument in arguments) 
+                startInfo.ArgumentList.Add(argument);
+
+            try
+            {
+                Process.Start(startInfo);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+    
+        public static bool IsAdmin() => isAdmin.Value;
+    }
+}
